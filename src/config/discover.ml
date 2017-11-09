578b789d77aba5a187b2f38c8a7c5a993291c426
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-11-09 16:08:12 +0800

    Introduce jbuilder/configurator based build system

---
+++ src/config/discover.ml
@@ -0,0 +1,21 @@
+open Base
+open Stdio
+
+let () =
+  let module C = Configurator in
+  C.main ~name:"zmq" (fun c ->
+    let default : C.Pkg_config.package_conf = {
+      libs = ["-lzmq"];
+      cflags = []
+    } in
+    let conf =
+      match C.Pkg_config.get c with
+      | None -> default
+      | Some pc ->
+          Option.value (C.Pkg_config.query pc ~package:"libzmq") ~default
+    in
+    let write_sexp file sexp =
+      Out_channel.write_all file ~data:(Sexp.to_string sexp)
+    in
+    write_sexp "c_flags.sexp" (sexp_of_list sexp_of_string conf.cflags);
+    write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.libs))
