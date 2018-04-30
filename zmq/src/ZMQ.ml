a1e64d8a60a93ee404fcb9204d3cedaec1cf1db7
Hezekiah M. Carty <hez@0ok.org>
2018-04-30 14:11:20 -0700

    Add deprecation attribute to ZMQ

--- zmq/src/ZMQ.ml
+++ zmq/src/ZMQ.ml
@@ -1,1 +1,5 @@
+[@@@ocaml.deprecated
+  "ZMQ is deprecated and will be removed in a future release.  Please use the \
+   Zmq module instead."]
+
 include Zmq
--- zmq/src/ZMQ.mli
+++ zmq/src/ZMQ.mli
@@ -1,4 +1,10 @@
-include module type of struct include Zmq end
+[@@@ocaml.deprecated
+  "ZMQ is deprecated and will be removed in a future release.  Please use the \
+   Zmq module instead."]
+
+include module type of struct
+    include Zmq
+end
 
 (** @deprecated This module is deprecated and will be removed in future
     versions.  Please use the `Zmq` module instead. *)
